55fe024ae05d8947bb2931b066d5bfb5c0c91e77
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-08-12 20:05:12 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -11,7 +11,7 @@ class Person
       begin
         raise PartnerError
       rescue PartnerError => error
-        puts. error.message
+        puts error.message
     else
       person.partner = self
     end
