1315cacaebb861063d0dff5239ab8f6c34da8e79
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-08-12 20:05:02 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -8,7 +8,10 @@ class Person
   def get_married(person)
     self.partner = person
     if person.class != Person
-      raise PartnerError
+      begin
+        raise PartnerError
+      rescue PartnerError => error
+        puts. error.message
     else
       person.partner = self
     end
